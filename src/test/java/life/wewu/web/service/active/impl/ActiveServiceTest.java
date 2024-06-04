6fa040f36324c00124c28bc998f552cc9444cf40
sungnam cho <sung6081@gmail.com>
2024-06-04 17:02:42 +0900

    Feat : sung / 24.06.04
    Test 안올려서 다시 올림

---
+++ src/test/java/life/wewu/web/service/active/impl/ActiveServiceTest.java
@@ -0,0 +1,138 @@
+package life.wewu.web.service.active.impl;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import life.wewu.web.domain.active.Active;
+import life.wewu.web.service.active.ActiveService;
+
+@SpringBootTest
+public class ActiveServiceTest {
+
+	//필드
+	@Autowired
+	@Qualifier("activeServiceImpl")
+	private ActiveService activeService;
+	
+	//메소드
+	
+	//등록 테스트
+	//@Test
+	public void testAddActive() {
+		
+		System.out.println("addActive");
+		
+		Date date = new Date(new java.util.Date().getDate());
+		
+		Active active = Active.builder()
+				.groupNo(1)
+				.activeName("도봉산 쓰레기 줍기")
+				.activeStartDate(date)
+				.activeEndDate(date)
+				.activeX("37.5679981")
+				.activeY("126.9812957")
+				.activeStartTime("13:00:00")
+				.activeEndTime("15:00:00")
+				.activeInfo("쓰레기 봉투는 각자 가져오기!")
+				.activeLocal("도봉")
+				.stateFlag("N")
+				.activeUrl("https://kr.object.ncloudstorage.com/wewu-project-test/hun_balnom.jpg")
+				.activeShortUrl("https://me2.do/Fu6RPnkN")
+				.build();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("active", active);
+		map.put("hash", "도봉산,플로깅,쓰레기 줍기");
+		
+		activeService.addActive(map);
+		
+	}
+	
+	//조회 테스트
+	//@Test
+	public void testGetActive() {
+		
+		System.out.println("getActive");
+		
+		Active active = activeService.getActive(3);
+		
+		System.out.println("getActive Done");
+		
+		System.out.println(active);
+		
+	}
+	
+	//업데이트 테스트
+	//@Test
+	public void testUpdateActive() {
+		
+		System.out.println("updateActive");
+		
+		System.out.println("업데이트 전");
+		
+		Active active = activeService.getActive(3);
+		
+		System.out.println(active);
+		
+		active.setActiveName("도봉산 플로깅");
+		active.setActiveStartTime("10:00:00");
+		active.setActiveEndTime("12:00:00");
+		active.setActiveInfo("플로깅 합시다");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("active", active);
+		//map.put("hash", "도봉산,플로깅,쓰레기 줍기");
+		map.put("hash", "도봉산, 플로깅");
+		
+		activeService.updateActive(map);
+		
+		System.out.println();
+		System.out.println("update 완료");
+		active = activeService.getActive(3);
+		System.out.println(active);
+		
+	}
+	
+	//삭제 테스트
+	//@Test
+	public void testDeleteActive() {
+		
+		System.out.println("deleteActive");
+		
+		System.out.println("delete 전");
+		
+		Active active = activeService.getActive(3);
+		
+		System.out.println();
+		
+		activeService.deleteActive(active);
+		
+		System.out.println("delete 후");
+		
+		active = activeService.getActive(3);
+		
+		System.out.println(active);
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
