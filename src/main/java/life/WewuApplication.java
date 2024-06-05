3ab8f81cb0b1365665af472ff4867612cc904299
Jiwon <dtbsmf01@gmail.com>
2024-06-05 20:05:32 +0900

    Feat : won / 24.06.05
    - properties수정
    - pom.xml수정

--- src/main/java/life/WewuApplication.java
+++ src/main/java/life/WewuApplication.java
@@ -2,8 +2,10 @@ package life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value="classpath:static/common/common.properties")
 public class WewuApplication {
 
 	public static void main(String[] args) {
