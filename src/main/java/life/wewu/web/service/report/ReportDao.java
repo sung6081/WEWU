530ee3b22ca1bedae67b5927a337ad3c88f521d0
chu5491 <chu5491@gmail.com>
2024-06-06 14:02:20 +0900

    Feat : chu / 24.06.06 Report 	report 도메인 작업 ReportDao 	report B/L Mybatis 선언 완료 reportMapper 	report 관련 쿼리 작성 완료 ReportService 	report 관련 메소드 선언 ReportServiceImpl 	report 관련 메소드 구현완료 ReportServiceTest 	report 관련 메소드 및 MyBatis 테스트 완료

--- src/main/java/life/wewu/web/service/report/ReportDao.java
+++ src/main/java/life/wewu/web/service/report/ReportDao.java
@@ -1,5 +1,22 @@
 package life.wewu.web.service.report;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import life.wewu.web.common.Search;
+import life.wewu.web.domain.report.Report;
+
+@Mapper
 public interface ReportDao {
 
+	public void addReport(Report report) throws Exception;
+	
+	public Report getReport(int reportNo) throws Exception;
+	
+	public List<Report> getReportList(Search search) throws Exception;
+	
+	public void updateReport(Report report) throws Exception;
+	
+	public void deleteReport(int reportNo) throws Exception;
 }
